177554776fa065e8077b642d560b7956d5f9cbe5
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-05-12 14:47:50 +0200

    rename files add underscores to improve readability add imports to systems init

---
+++ pyturbo/systems/__init__.py
@@ -0,0 +1,4 @@
+from pyturbo.systems.combustor_simple_aero import CombustorSimpleAero
+from pyturbo.systems.compressor_mft_aero import CompressorMftAero
+from pyturbo.systems.compressor_simple_aero import CompressorSimpleAero
+from pyturbo.systems.turbine_simple_aero import TurbineSimpleAero
