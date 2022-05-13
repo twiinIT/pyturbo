5a866ad54a810415b8928e76a96e62ea93b17d90
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-05-13 17:45:30 +0200

    add a generic simple geometry derive a compressor simple geom from the generic one add a demo notebook add new pyoccad and pythreejs deps

---
+++ pyturbo/systems/compressor_simple_geom.py
@@ -0,0 +1,11 @@
+from pyturbo.systems.generic_simple_geom import GenericSimpleGeom
+
+
+class CompressorSimpleGeom(GenericSimpleGeom):
+    """Compressor geometry.
+
+    The inlet and exit stations are assumed fully radial and the axial length
+    is computed from a simple form factor
+    """
+
+    pass
