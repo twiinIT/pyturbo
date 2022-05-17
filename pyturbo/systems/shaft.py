7b86ee5fc84d49de938886e5acdd93c0f1b32a6b
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-05-17 16:12:20 +0200

    improve readability

--- pyturbo/systems/shaft.py
+++ pyturbo/systems/shaft.py
@@ -13,6 +13,9 @@ class Shaft(System):
 
     It forwards the power received at input(s) to output(s) with a given
     transmission efficiency.
+    If multiple shaft outputs are created, the power fraction is automatically
+    created using the same sequence as the model `output_shafts` argument.
+
     Note: a power extraction can be simply added using an output shaft port.
     """
 
@@ -25,39 +28,41 @@ class Shaft(System):
         for p in input_shafts:
             self.add_input(ShaftPort, p)
 
+        for p in output_shafts:
+            self.add_output(ShaftPort, p)
+
         outputs_count = len(output_shafts)
         has_many_outputs = outputs_count > 1
         self.add_property("has_many_outputs", has_many_outputs)
 
+        self.add_inward("eff", 0.995, desc="transmission efficiency")
+        self.add_inward("fraction_scalar", 1.0, unit="", desc="scalar on all fraction")
+        self.add_inward("power_fractions", np.ones(len(output_shafts)) / outputs_count)
         self.add_outward("total_input_power", 1.0, unit="W")
         self.add_outward("total_output_power", 1.0, unit="W")
 
+        # off-design equations/unknowns
         if has_many_outputs:
-            self.add_inward("fraction_scalar", 1.0, unit="", desc="scalar on all fraction")
             self.add_unknown("fraction_scalar").add_equation(
                 "total_input_power == total_output_power"
             )
 
-        for i, p in enumerate(output_shafts):
-            self.add_output(ShaftPort, p)
-            self.add_inward(f"{p}_power_fraction", 1 / outputs_count)
-
-        # additional inwards/outwards
-        self.add_inward("eff", 0.995, desc="transmission efficiency")
-
     def compute(self):
+        shaft_in_ports = [p for p in self.inputs.values() if type(p) == ShaftPort]
+        shaft_out_ports = [p for p in self.outputs.values() if type(p) == ShaftPort]
+
         # this is user responsability to add equations/unknowns to ensure consistency
-        mean_input_N = np.mean([p.N for p in self.inputs.values() if type(p) == ShaftPort])
+        mean_input_N = np.mean([p.N for p in shaft_in_ports])
+        self.total_input_power = np.sum([p.power for p in shaft_in_ports])
 
-        self.total_input_power = np.sum(
-            [p.power for p in self.inputs.values() if type(p) == ShaftPort]
+        shaft_out_powers = (
+            np.sum([p.power for p in shaft_in_ports])
+            * self.power_fractions
+            * self.fraction_scalar
+            * self.eff
         )
+        self.total_output_power = np.sum(shaft_out_powers)
 
-        self.total_output_power = 0.0
-        for p in [p for p in self.outputs.values() if type(p) == ShaftPort]:
+        for i, p in enumerate(shaft_out_ports):
             p.N = mean_input_N
-            fraction = self[f"{p.name}_power_fraction"]
-            if self.has_many_outputs:
-                fraction *= self.fraction_scalar
-            p.power = self.total_input_power * fraction * self.eff
-            self.total_output_power += p.power
+            p.power = shaft_out_powers[i]
