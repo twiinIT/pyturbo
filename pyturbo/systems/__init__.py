e9eb28f4f85e814163ba694db969f50597539f2a
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-05-20 09:02:42 +0200

    add nacelle geom model add demo notebook

--- pyturbo/systems/__init__.py
+++ pyturbo/systems/__init__.py
@@ -3,6 +3,7 @@ from pyturbo.systems.compressor_mft_aero import CompressorMftAero
 from pyturbo.systems.compressor_simple_aero import CompressorSimpleAero
 from pyturbo.systems.compressor_simple_geom import CompressorSimpleGeom
 from pyturbo.systems.inlet_simple_aero import InletSimpleAero
+from pyturbo.systems.nacelle_simple_geom import NacelleSimpleGeom
 from pyturbo.systems.nozzle_simple_aero import NozzleSimpleAero
 from pyturbo.systems.shaft import Shaft
 from pyturbo.systems.turbine_simple_aero import TurbineSimpleAero
