d90228fafa3d950a8e72618c477b50f0ed071d0d
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-05-21 21:57:13 +0200

    add turbine models add aero, geom and assembly models move turbine aero model to submodule

--- pyturbo/systems/__init__.py
+++ pyturbo/systems/__init__.py
@@ -6,6 +6,6 @@ from pyturbo.systems.inlet_simple_aero import InletSimpleAero
 from pyturbo.systems.nacelle_simple_geom import NacelleSimpleGeom
 from pyturbo.systems.nozzle_simple_aero import NozzleSimpleAero
 from pyturbo.systems.shaft import Shaft
-from pyturbo.systems.turbine_simple_aero import TurbineSimpleAero
+from pyturbo.systems.turbine import TurbineSimple, TurbineSimpleAero, TurbineSimpleGeom
 
 from pyturbo.systems.gas_generator_simple_aero import GasGeneratorSimpleAero  # isort: skip
---
+++ pyturbo/systems/turbine/__init__.py
@@ -0,0 +1,4 @@
+from pyturbo.systems.turbine.turbine_simple_aero import TurbineSimpleAero
+from pyturbo.systems.turbine.turbine_simple_geom import TurbineSimpleGeom
+
+from pyturbo.systems.turbine.turbine_simple import TurbineSimple  # isort: skip
