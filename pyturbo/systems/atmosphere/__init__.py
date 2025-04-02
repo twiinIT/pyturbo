28803c0ba2af483c34b125a2ca8a049522283179
Haris Musaefenidc <haris.musaefendic@gmail.com>
2025-04-02 18:28:50 +0200

    replace relative import (#93)

--- pyturbo/systems/atmosphere/__init__.py
+++ pyturbo/systems/atmosphere/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2022-2023, twiinIT
 # SPDX-License-Identifier: BSD-3-Clause
 
-from .atmosphere import Atmosphere
+from pyturbo.systems.atmosphere.atmosphere import Atmosphere
 
 __all__ = ["Atmosphere"]
