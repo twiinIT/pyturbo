54842cb7851255a8616530b86e9011fd1b6153b6
Guy De Spiegeleer <guy.de-spiegeleer@twiinit.com>
2025-04-16 14:47:26 +0200

    more general init_environment

--- pyturbo/thermo/init_environment.py
+++ pyturbo/thermo/init_environment.py
@@ -2,21 +2,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import ambiance
-
 from cosapp.base import System
+
 from pyturbo.thermo import IdealDryAir
 
 
-def init_environment(sys: System, alt : float = 0.0, mach : float = 0.0, dtamb : float = 0.0):
+def init_environment(
+    sys: System, fl_name: str = "fl_in", alt: float = 0.0, mach: float = 0.0, dtamb: float = 0.0
+):
     """Init fluid data with mission values."""
-    
+
     gas = IdealDryAir()
     atm = ambiance.Atmosphere(alt)
     pamb = atm.pressure[0]
     tamb = atm.temperature[0] + dtamb
 
-    sys.pamb = pamb
-    sys.fl_in.Tt = gas.total_t(tamb, mach)
-    sys.fl_in.Pt = gas.total_p(pamb, tamb, sys.fl_in.Tt)
+    if "pamb" in sys:
+        sys.pamb = pamb
+
+    sys[fl_name].Tt = gas.total_t(tamb, mach)
+    sys[fl_name].Pt = gas.total_p(pamb, tamb, sys[fl_name].Tt)
 
     return sys
